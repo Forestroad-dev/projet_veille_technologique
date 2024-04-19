b9619d8ac89ab086f549292b33927a33c59bbc69
ouzeuh <127106005+ouzeuh@users.noreply.github.com>
2024-04-19 18:17:49 +0000

    Add files via upload

---
+++ modele/call_model.py
@@ -0,0 +1,41 @@
+import pickle
+import pandas as pd
+import numpy as np
+from pymongo import MongoClient
+
+# Connexion à MongoDB
+client = MongoClient('localhost', 27017)
+db = client['next-amazona-v2']
+collection = db['products']
+
+# Charger le modèle pré-entraîné
+with open('modele_v2.pkl', 'rb') as f:
+    similarity_vectors = pickle.load(f)
+
+# Charger le DataFrame 'data'
+# (Assurez-vous que le chemin d'accès au fichier est correct)
+#data = pd.read_csv('chemin_vers_votre_fichier.csv')  # Remplacez 'chemin_vers_votre_fichier.csv' par le chemin d'accès à votre fichier CSV
+
+# Récupération des données de la base MongoDB
+data = pd.DataFrame(list(collection.find()))
+
+# Suppression des lignes avec des valeurs manquantes
+data = data.dropna(subset=['category', 'brand', 'rating', 'description'])
+
+# Développement du moteur de recommandation
+def recommender(search):
+    matching_products = data[data['name'].str.contains(search, case=False, na=False)]
+    
+    if not matching_products.empty:
+        idx = matching_products.index[0]
+        similar_products_indices = np.argsort(similarity_vectors[idx])[::-1][1:6]
+        
+        # Afficher seulement les colonnes 'product_id', 'name' et 'slug'
+        return data.iloc[similar_products_indices][['name', 'category','price','rating']]
+        #return data.iloc[similar_products_indices]
+
+    else:
+        return "Aucun produit trouvé dans la base de données correspondant à votre recherche. Veuillez essayer avec d'autres termes."
+
+# Appel de la fonction recommender
+print(recommender('shirt'))
